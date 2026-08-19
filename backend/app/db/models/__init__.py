155b2e07c7707d2075e1e03df9248de33caef9b9
Prapti Sharma <122536321+Prapti-429@users.noreply.github.com>
2026-08-19 10:29:42 +0530

    feat: register past history models

--- backend/app/db/models/__init__.py
+++ backend/app/db/models/__init__.py
@@ -1,35 +1,22 @@
 from app.db.models.patient import PatientProfile
 from app.db.models.user import User
-
 from app.db.models.audio_recording import AudioRecording
 from app.db.models.video_recording import VideoRecording
 from app.db.models.biomarker_feature import BiomarkerFeature
 from app.db.models.daily_check_in import DailyCheckIn
 from app.db.models.health_stability_score import HealthStabilityScore
 from app.db.models.symptom_report import SymptomReport
-
 from app.db.models.medication import MedicationRegimen, MedicationAdherenceLog
 from app.db.models.vitals import VitalSigns
 from app.db.models.labs import LabResult
 from app.db.models.symptoms import SymptomLog
 from app.db.models.lifestyle import NutritionLog, LifestyleLog
 from app.db.models.file_upload import FileUploadRecord
+from app.db.models.past_history import PastHistoryRecord, MedicalDocument, HealthReminder
 
 __all__ = [
-    "User",
-    "PatientProfile",
-    "AudioRecording",
-    "VideoRecording",
-    "BiomarkerFeature",
-    "DailyCheckIn",
-    "HealthStabilityScore",
-    "SymptomReport",
-    "MedicationRegimen",
-    "MedicationAdherenceLog",
-    "VitalSigns",
-    "LabResult",
-    "SymptomLog",
-    "NutritionLog",
-    "LifestyleLog",
-    "FileUploadRecord",
+    "User", "PatientProfile", "AudioRecording", "VideoRecording", "BiomarkerFeature",
+    "DailyCheckIn", "HealthStabilityScore", "SymptomReport", "MedicationRegimen",
+    "MedicationAdherenceLog", "VitalSigns", "LabResult", "SymptomLog", "NutritionLog",
+    "LifestyleLog", "FileUploadRecord", "PastHistoryRecord", "MedicalDocument", "HealthReminder",
 ]
